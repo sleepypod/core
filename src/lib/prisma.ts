f4d229c8e7f95c886d89fb7055dc4f21911e1d93
Jonathan Ng <jon@jonathanng.com>
2025-12-15 20:05:14 -0800

    feat(db): add and configure prisma (#18)

---
+++ src/lib/prisma.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from '@/generated/prisma/client'
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
+
+const connectionString = `${process.env.DATABASE_URL}`
+
+const adapter = new PrismaBetterSqlite3({ url: connectionString })
+const prisma = new PrismaClient({ adapter })
+
+export { prisma }
