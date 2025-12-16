4997c31747ea2025a4dbb284d89eebf06d9d996e
Jonathan Ng <jon@jonathanng.com>
2025-12-15 19:51:24 -0800

    initial prisma config

---
+++ prisma.config.ts
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+import { defineConfig, env } from "prisma/config";
+
+type Env = {
+  DATABASE_URL: string
+}
+
+const targetEnv = process.env.NODE_ENV || "dev"
+dotenv.config({
+  path: `.env.${targetEnv}`
+});
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    url: env<Env>("DATABASE_URL"),
+  },
+});
