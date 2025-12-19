74b7c27ab6c4607f58971642a51b3458a839e99c
Jonathan Ng <jon@jonathanng.com>
2025-12-18 22:53:58 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,24 @@
-import { TRPCProvider } from '@/src/components/providers/TRPCProvider' // Our client component TRPC wrapper
-import type { Metadata } from 'next'
+import { BottomNav } from '@/components/BottomNav/BottomNav';
+import { TRPCProvider } from '@/src/components/providers/TRPCProvider';
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono, Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
 
 export const metadata: Metadata = {
-  title: 'MyApp',
-  description: 'Global description',
+  title: 'sleepypod',
 }
 
 // This layout does not rely on the `lang` param
@@ -13,18 +28,26 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html>
+    <html className={inter.variable}>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
 
-      <body>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         {/* TRPCProvider should be placed here (high up) to maintain its state/cache
             across locale changes (e.g., /en/page -> /ar/page) */}
         <TRPCProvider>
-          {children}
+          <div className="min-h-screen bg-black text-white flex flex-col items-center pb-24">
+            <div className="w-full max-w-md px-4 pt-4 space-y-6">
+              {children}
+
+              <BottomNav />
+            </div>
+          </div>
         </TRPCProvider>
       </body>
-    </html>
+    </html >
   )
 }
