1f3cf7eb867f953315808f4679da8558dc97c133
Jonathan Ng <jon@jonathanng.com>
2025-12-18 22:54:01 -0800

    Update page.tsx

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,14 +1,12 @@
-// You now have access to the current locale
-
 import { Trans } from '@lingui/react/macro'
 
-// e.g. /en-US/products -> `lang` is "en-US"
 export default async function Page({
   params,
 }: {
   params: Promise<{ lang: string }>
 }) {
   const lang = (await params).lang
+
   return (
     <div>
       <h1>
